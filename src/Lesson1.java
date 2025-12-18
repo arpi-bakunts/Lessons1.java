8d906850b37b21a4e974b7d608decac656b822f1
arpine-bakunts <arpine.bakunc2203@gmail.com>
2025-12-19 01:53:50 +0400

    Add Java homwork with primitive operations

---
+++ src/Lesson1.java
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+public class Lesson1 {
+    public static void main(String[] args) {
+        System.out.println("Barev");
+        System.out.println("Lesson1");
+
+        byte x = 127;
+        System.out.println(x);
+        short y = -4542;
+        System.out.println(y);
+        int i = 1000;
+        System.out.println(i);
+        long l = 100000L;
+        System.out.println(l);
+        float f = 5.5f;
+        System.out.println(f);
+        double d = 10.25;
+        System.out.println(d);
+        char c ='A';
+        System.out.println(c);
+        boolean v = true;
+        System.out.println(v);
+
+
+
+    }
+}
+
+
