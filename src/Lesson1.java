fc6f60de9337acc4d5a468e8268025d1b00cab46
arpine-bakunts <arpine.bakunc2203@gmail.com>
2025-12-19 12:07:03 +0400

    Added program to calculate cube of h+o

--- src/Lesson1.java
+++ src/Lesson1.java
@@ -19,6 +19,32 @@ public class Lesson1 {
         System.out.println(c);
         boolean v = true;
         System.out.println(v);
+int a = 30;
+int b = 35;
+int c2 = 36;
+double average = (a+b+c2) / 3.0;
+double decimaltPart = average - (int) average;
+        System.out.println("average value is: " + average);
+        System.out.println("Decimal part of the number is : " + "decimalPart");
+        System.out.println("Միջին թվաբանականը (ընդամենը ամբողջ թիվ)՝ " + average);
+        int integerPart = (int) average;
+        double fractionalPart = average - integerPart;
+        System.out.println("Տասնորդական մասը՝ " + fractionalPart);
+        int s = 3;
+        int k = 5;
+        int m = 7;
+        int threeDigitNumber = s * 100 + k * 10 + m;
+        System.out.println("Ստացված եռանիշ թիվը՝ " + threeDigitNumber);
+        int h = 2;
+        int o = 3;
+        int sum = h + o;
+        int cube = sum * sum * sum;
+
+        System.out.println(cube);
+
+
+
+
 
 
     }
